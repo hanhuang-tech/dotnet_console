8569775a63d8fc902b305a3903ff14941a10340c
hanhuang <han@hanhuang.tech>
2022-05-23 15:53:52 +1200

    Add new function to WeatherApp

---
+++ WeatherApp/Functions/MinVarTemp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WeatherApp.Functions
+{
+    internal class MinVarTemp
+    {
+        public static void MinVarTempCalc(string file)
+        {
+            Console.WriteLine("-Using " + file);
+            string[] lines = File.ReadAllLines(file);
+            foreach (var line in lines)
+            {
+                //var value = line.Split(new string[] { }, StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using WeatherApp.Functions;
 
 namespace WeatherApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            //locate input file
+            string file = Path.Combine(Application.StartupPath, @"..\..\weather.txt");
+
+            Console.WriteLine("- WeatherApp -");
+            Console.WriteLine("Please select a function you wish to action");
+            Console.WriteLine("1) MinVarTempCalc 2) Something else");
+            string ynRead = Console.ReadLine();
+            if (ynRead == "1")
+            {
+                MinVarTemp.MinVarTempCalc(file);
+            }
+            else if (ynRead == "2")
+            {
+                Console.WriteLine("Sorry, there is nothing here. Start again?");
+                Console.WriteLine(Environment.NewLine);
+                Main();
+            }
+            else
+            {
+                Console.WriteLine("!Invalid input, please try again");
+                Console.WriteLine(Environment.NewLine);
+                Main();
+            }
+
         }
     }
 }
+

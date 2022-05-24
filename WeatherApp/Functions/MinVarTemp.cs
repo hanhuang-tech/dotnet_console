b94f84ca6c49828f769d26e16ff8e20840530a49
hanhuang <han@hanhuang.tech>
2022-05-25 00:41:28 +1200

    Add new 2d Array to class

--- WeatherApp/Functions/MinVarTemp.cs
+++ WeatherApp/Functions/MinVarTemp.cs
@@ -1,20 +1,32 @@
 ﻿using System;
 using System.IO;
-using System.Windows.Forms;
+using System.Linq;
 
 namespace WeatherApp.Functions
 {
     internal class MinVarTemp
     {
-        public static void MinVarTempCalc(string file)
+        public static void MinVarTempCalc(string path)
         {
-            Console.WriteLine("-Using " + file);
-            string[] lines = File.ReadAllLines(file);
-            foreach (var line in lines)
+            Console.WriteLine("-Using " + path);
+
+            string[] lines = File.ReadAllLines(path);   //all lines in table
+            string[] lcontent; //content on each line
+            string[][] arr = new string[lines.Count() + 1][];   //
+            int k = 1;
+
+            foreach (string line in lines)
             {
-                //var value = line.Split(new string[] { }, StringSplitOptions.RemoveEmptyEntries);
-                Console.WriteLine(line);
+                lcontent = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 1; i < lcontent.Length; i++)
+                {
+                    if (arr[k] == null) { arr[k] = new string[lcontent.Length + 1]; }
+                    arr[k][i] = lcontent[i];
+                }
+                k++;
             }
+
+            Console.WriteLine(arr[10][3]);
         }
     }
 }

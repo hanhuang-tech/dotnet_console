1f4a108187eb6257f5997f9146523bb240b3e635
hanhuang <han@hanhuang.tech>
2022-06-28 14:23:14 +1200

    Remove adjacent project inside solution

---
+++ CastingNum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+public static class Program
+{
+    static void Main()
+    {
+        using (StreamReader reader = new StreamReader(Console.OpenStandardInput()))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                Console.WriteLine(line);
+            }
+    }
+}
